83bd102dcc320209abd48e6286ee209fe9708497
Abirami16 <abiramibalu16@gmail.com>
2016-07-23 12:05:57 +0530

    jhghjgk
    gfjhkg

---
+++ EvenNumber1.java
@@ -0,0 +1,18 @@
+
+public class EvenNumber {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+ int number1=Integer.parseInt(args[0]);
+ int number2=Integer.parseInt(args[1]);
+ System.out.println("the range of odd number is");
+ for(int i=number1;i<number2;i++)
+ {
+	 if(i%2==0)
+	 {
+		 System.out.println(i);
+	 }
+ }
+	}
+
+}
