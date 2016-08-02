77eb1d542a1d191420365e1e76d6be834795ed5a
Abirami16 <abiramibalu16@gmail.com>
2016-08-01 22:34:45 -0700

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,12 @@
+
+public class Fact {
+public static void main(String[] args) {
+	int number=Integer.parseInt(args[0]);
+	int fact=1;
+	for(int i=1;i<=number;i++)
+	{
+		fact=fact*i;
+	}
+	System.out.println("The factorial is " +fact);
+}
+}
