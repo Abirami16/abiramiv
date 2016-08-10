ae59a6a71c2dc8b9d81773d5e7bb4f027dcb75e2
Abirami16 <abiramibalu16@gmail.com>
2016-08-09 23:29:47 -0700

    Add files via upload

--- ArrayCheck.java
+++ ArrayCheck.java
@@ -1,25 +1,33 @@
 import java.util.Scanner;
-
-
 public class ArrayCheck {
-
 	public static void main(String[] args) {
+		
 		Scanner s=new Scanner(System.in);
-		System.out.println("Enter the size of the array");
-		int size=s.nextInt();
-		int arr[]=new int[size];
-		System.out.println("Enter the elements");
-		for(int i=0;i<size;i++)
+		System.out.println("Enter the string");
+		String st=s.next();
+		int flag=0;
+		StringBuffer st1=new StringBuffer(st);
+		String str=st1.reverse().toString();
+		String str1="";
+		for(int i=0;i<str.length();i++)
 		{
-			arr[i]=s.nextInt();
-		}
-		for(int i=0;i<size;i++)
-		{
-			if(arr[i]==i)
+			flag=0;
+			for(int j=i+1;j<str.length();j++)
 			{
-				System.out.println("The number "+arr[i]+" is same as the index position "+i);
+				if(str.charAt(i)==str.charAt(j) )
+				{
+					flag++;
+				}
+			}
+			if(flag<1)
+			{
+				str1=str.charAt(i)+str1;
+				
 			}
 		}
+		System.out.println("The string after eliminating duplicate :"+str1);
+	
+	   }
 	}
 
-}
+
