358016d05c5222c6a15003fbecdea43c1e88cf99
Abirami16 <abiramibalu16@gmail.com>
2016-09-28 20:30:34 +0530

    Add files via upload

---
+++ ChildrenPair.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class ChildrenCount
+{
+	
+
+public static void main (String[] args)
+ 
+	{
+
+        
+        Scanner s=new Scanner(System.in);
+      
+  int childrenCount=s.nextInt();
+
+      
+  int pairOfChildren=0;
+
+        
+
+        for(int i=0;i<2*childrenCount;i++)
+
+       
+  {
+
+            
+  if(i!=2*childrenCount-1)
+
+          
+     {
+               
+     
+               System.out.println(++pairOfChildren);
+
+            
+   }
+
+        
+  }
+
+
+        
+
+     }
+
+
+
+
+}
