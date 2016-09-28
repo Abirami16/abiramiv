36e8178bf3ed83069e23f701ca78a5adcd1f23ff
Abirami16 <abiramibalu16@gmail.com>
2016-09-28 21:03:34 +0530

    Add files via upload

---
+++ MaxCandies.java
@@ -0,0 +1,90 @@
+class Candies
+
+{
+public static void main (String[] args)
+ 
+{
+
+  
+  Scanner s=new Scanner(System.in);
+
+  int childrenCount=s.nextInt();
+
+  int rating[]=new int[childrenCount];
+
+  
+int temp=0;
+
+  
+
+  for(int i=0;i<childrenCount;i++)
+
+  {
+
+      rating[i]=s.nextInt();
+
+  }
+
+  
+  for(int i=0;i<childrenCount;i++)
+
+  {
+
+      for(int j=i+1;j<rating.length;j++)
+
+      {
+
+          if(rating[i]>rating[j])
+
+          {
+ 
+          temp=rating[j];
+
+          rating[j]=rating[i];
+
+          rating[i]=temp;
+
+          }
+
+      }
+
+  }
+
+  
+  int sum=0;
+
+  int val=1;
+
+  
+  for(int i=0;i<rating.length;i++)
+
+  {
+
+      
+if(i>=1)
+
+      {
+
+          if(rating[i]!=rating[i-1])
+
+          {
+
+             val++;
+ 
+          }
+
+      }
+
+      sum=sum+val;
+
+   }
+
+  
+   System.out.println(sum);
+
+ }
+
+
+
+
+}
