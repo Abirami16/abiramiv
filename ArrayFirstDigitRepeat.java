badf5991ba56e9a5883bbe02d46ed40a74ef9fe3
Abirami16 <abiramibalu16@gmail.com>
2016-11-01 21:25:56 +0530

    Add files via upload

---
+++ ArrayFirstDigitRepeat.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+class ArrayNumbers
+{
+
+	public static void main (String[] args)
+	{
+
+		Scanner s=new Scanner(System.in);
+
+		int sizeOfArray=s.nextInt();
+
+		int arrayElements[]=new int[sizeOfArray];
+
+		for(int i=0;i<sizeOfArray;i++)
+
+		{
+
+		    arrayElements[i]=s.nextInt();
+
+		}
+
+		int count=0;
+
+		for(int i=0;i<sizeOfArray;i++)
+
+		{
+
+		    count=0;
+
+                   for(int j=i+1;j<sizeOfArray;j++)
+
+		   {
+
+		     if(arrayElements[i]==arrayElements[j])
+
+		     {
+
+		      count=1;
+
+		      break;
+
+		     }
+
+	            }
+
+	           if(count==1)
+
+       	           {
+
+	              System.out.println(arrayElements[i]);
+
+	              break;
+
+        	   }
+
+		}
+
+	    }
+
+     }
