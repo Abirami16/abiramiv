42cae2091ebd85be693a0a2408c9790953c9fc8b
Abirami16 <abiramibalu16@gmail.com>
2016-11-05 19:31:14 +0530

    Add files via upload

---
+++ Stringpalindrome.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Stringpalindrome
+{
+
+	public static void main (String[] args)
+ 
+	{
+
+	        Scanner s=new Scanner(System.in);
+
+		String inputString=s.next();
+
+		int length1=inputString.length();
+
+		String subString1="";
+
+		String subString2="";
+
+		int check=0;
+
+		int indexposition=0;
+
+		for(int i=0;i<length1/2;i++)
+
+		{
+
+		subString1=inputString.charAt(i)+"";
+
+		subString2=inputString.charAt(length1-1-i)+"";
+
+		if(!(subString1.equals(subString2)))
+
+		{
+
+		    indexposition=length1-1-i;
+
+		    check=1;
+
+		    break;
+
+		}
+
+		}
+
+		if(check==1)
+
+		{
+
+		    System.out.println(indexposition);
+
+		}
+
+	}
+
+}
